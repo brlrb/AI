ea2a4b4c342cdc466a43e952c879b33ad7558603
brlrb <bikramcode@gmail.com>
2019-06-18 10:34:16 -0700

    Formula to calculate Cross Entropy

---
+++ NeuralNetworks/CrossEntropy.py
@@ -0,0 +1,16 @@
+# UDACITY: Intro to Deep Learning with PyTorch 
+# Writing formula for Cross Entropy
+
+import numpy as np
+
+# Write a function that takes as input two lists Y, P,
+# and returns the float corresponding to their cross-entropy.
+def cross_entropy(Y, P):
+    Y = np.float_(Y)
+    P = np.float_(P)
+    return -np.sum(Y * np.log(P) + (1 - Y) * np.log(1 - P))
+
+
+
+
+print(cross_entropy([1,0,1,1], [0.4,0.6,0.1,0.5]))
