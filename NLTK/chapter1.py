5a1c276f4d9b47e24197fb723c750cdb86971686
brlrb <bikramcode@gmail.com>
2020-02-14 21:15:32 -0600

    NLTK Book chapter 1

---
+++ NLTK/chapter1.py
@@ -0,0 +1,65 @@
+import nltk
+from nltk.book import *
+
+
+print(""" Question 1 """)
+print(""" Try using the Python interpreter as a calculator, and typing expressions like 12 / (4 + 1). """)
+print(12 / (4 + 1))
+
+
+print(""" Question 2 """)
+print(""" Given an alphabet of 26 letters, there are 26 to the power 10, or 26 ** 10, ten-letter strings we can form. 
+        That works out to 141167095653376. How many hundred-letter strings are possible? """ """  """)
+
+
+
+print(""" Question 3 """)
+print(""" The Python multiplication operation can be applied to lists. 
+        What happens when you type ['Monty', 'Python'] * 20, or 3 * sent1? """)
+
+print(""" Question 4 """)
+print(""" Review 1 on computing with language. How many words are there in text2? 
+        How many distinct words are there? """)
+
+print(""" Question 5 """)
+print(""" Compare the lexical diversity scores for humor and romance fiction in 1.1. 
+        Which genre is more lexically diverse? """)
+
+print(""" Question 6 """)
+print(""" Produce a dispersion plot of the four main protagonists in 
+        Sense and Sensibility: Elinor, Marianne, Edward, and Willoughby. 
+        What can you observe about the different roles played by the males and 
+        females in this novel? Can you identify the couples? """)
+
+print(""" Question 7 """)
+print(""" Find the collocations in text5. """)
+
+print(""" Question 8 """)
+print(""" Consider the following Python expression: len(set(text4)). 
+        State the purpose of this expression. 
+        Describe the two steps involved in performing this computation. """)
+
+print(""" Question 9 """)
+print(""" Review 2 on lists and strings. """)
+print(""" a: Define a string and assign it to a variable, e.g., my_string = 'My String' 
+        (but put something more interesting in the string). 
+        Print the contents of this variable in two ways, first by simply typing the variable name and 
+        pressing enter, then by using the print statement. """)
+
+print(""" b: Try adding the string to itself using my_string + my_string, or multiplying 
+        it by a number, e.g., my_string * 3. Notice that the strings are joined together without any spaces. 
+        How could you fix this? """)
+
+print(""" Question 10 """)
+print(""" Define a variable my_sent to be a list of words, using the syntax my_sent = ['My', 'sent'] 
+        (but with your own words, or a favorite saying). """)
+print(""" a: Use ' '.join(my_sent) to convert this into a string. """)
+
+print(""" b: Use split() to split the string back into the list form you had to start with. """)
+
+
+
+
+
+
+
